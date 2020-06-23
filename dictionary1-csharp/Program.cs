a9e7161117850315cd4bdcc0984c4137cff2f51a
Gustavo Gomes <estagiarios@grtsolucoes.com.br>
2020-06-22 23:44:37 -0300

    Dictionary Exercise

---
+++ dictionary1-csharp/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.Write("Enter file full path: ");
+            string path = Console.ReadLine();
+
+            try
+            {
+                Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+                using (StreamReader votesReader = File.OpenText(path))
+                {
+                    while (!votesReader.EndOfStream)
+                    {
+                        string[] votingRecord = votesReader.ReadLine().Split(',');
+                        string candidate = votingRecord[0];
+                        int votes = int.Parse(votingRecord[1]);
+
+                        if (dictionary.ContainsKey(candidate))
+                        {
+                            dictionary[candidate] += votes;
+                        }
+                        else
+                        {
+                            dictionary[candidate] = votes;
+                        }
+                    }
+
+                    foreach (KeyValuePair<string, int> item in dictionary)
+                    {
+                        Console.WriteLine($"{item.Key},{item.Value}");
+                    }
+                }
+            }
+            catch (IOException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+            catch (ArgumentException err)
+            {
+                Console.WriteLine(err.Message);
+            }
+        }
+    }
+}
